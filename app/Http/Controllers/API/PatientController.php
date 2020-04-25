256371af8f9be89736e97014803ccdf4d7191a47
fuadmmnf <bsse0920@iit.du.ac.bd>
2020-04-25 20:25:17 +0600

    patient backend api controller

---
+++ app/Http/Controllers/API/PatientController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Patient;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class PatientController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required| min:1',
+            'mobile' => 'required| min:11',
+            'password' => 'required| min:8',
+        ]);
+
+        $newPatient = new Patient();
+        $newPatient->name = $request->name;
+        $newPatient->mobile = $request->mobile;
+        $newPatient->password = Hash::make($request->password);
+        $newPatient->save();
+
+        return response()->json($newPatient);
+    }
+
+
+    public function show($id)
+    {
+        $patient = Patient::findOrFail($id);
+        return response()->json($patient);
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+
+    public function destroy($id)
+    {
+        //
+    }
+}
