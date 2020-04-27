406cf7f725d5d21ad961890621a63a173ad6d77f
fuadmmnf <bsse0920@iit.du.ac.bd>
2020-04-27 17:43:33 +0600

    authentication controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Doctor;
+use App\Patient;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function authenticateUser(Request $request){
+        $this->validate($request, [
+            'mobile' => 'required| min:11',
+            'password' => 'required| min:8',
+            'device_id' => 'required',
+        ]);
+
+        $user = Patient::where('mobile', $request->mobile)
+            ->where('password', Hash::make($request->password))
+            ->first();
+
+        if(!$user){
+            $user = Doctor::where('mobile', $request->mobile)
+                ->where('password', Hash::make($request->password))
+                ->first();
+//            $user->is_doctor = true;
+        }
+        $user->isDoctor = false;
+
+        if($user)
+            return response()->json($user, 200);
+        else
+            return response()->json('Bad Credentials', 401);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,13 @@ use Illuminate\Support\Facades\Route;
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
+Route::post('authenticate', 'AuthController@authenticateUser');
+
+
 Route::apiResource('doctortypes', 'API\DoctorTypesController');
 Route::apiResource('patients', 'API\PatientController');
 
 Route::apiResource('doctors', 'API\DoctorController');
 Route::get('doctortypes/{doctortype_id}/doctors/available', 'API\DoctorController@getSingleAvailableDoctorByType');
+
+
